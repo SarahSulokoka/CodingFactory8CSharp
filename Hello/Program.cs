6e301651c94953e69d551fa138a55c58aa9063b7
Sara <sarahsulokoka@gmail.com>
2025-07-28 16:18:37 +0300

    initial commit for C# project

---
+++ Hello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello
+{
+    /// <summary>
+    /// comments
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("Hello, Coding Factory");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ OutputApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OutputApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10_123_153;
+            int num2 = 20_123_153;
+
+            Console.WriteLine("Num: " + num1);
+            Console.WriteLine("Num1: {0,6:N2}, Num2 : {1, C2} ", num1, num2);
+            Console.WriteLine($"Num1: {num1}, Num2 : {num2}");
+        }
+    }
+}
