926f52df524056120040a730ea34abeae2f736c0
Sara <sarahsulokoka@gmail.com>
2025-07-29 13:11:04 +0300

    FunctionalApp added

---
+++ FuctorialApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace FuctorialApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static BigInteger FactoIterative(int n)
+        {
+            BigInteger result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        public static BigInteger FactoRecursive(int n)
+        {
+            return (n > 1) ? n * FactoRecursive(n - 1) : 1;
+        }
+        
+    }
+}
