243cc86f42cf0f3028a423531a6b591695876241
Sara <sarahsulokoka@gmail.com>
2025-07-29 13:46:05 +0300

    Fibo With Array Added

---
+++ FiboWithArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FiboWithArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static int Fibo(int n)
+        {
+            int[] arr = new int[n + 1];
+            arr[0] = 0;
+            arr[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr[n];
+        }
+    }
+}
